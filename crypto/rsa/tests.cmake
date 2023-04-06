67c77c8cd3bfec70eab2e0cb156b4a2eeeb4532c
Fs <Fsu0413@vip.qq.com>
2023-04-06 08:49:21 +0800

    rsa and x509 time test

--- crypto/rsa/tests.cmake
+++ crypto/rsa/tests.cmake
@@ -1,4 +1,4 @@
 add_executable(rsa_test EXCLUDE_FROM_ALL ${CMAKE_SOURCE_DIR}/openssl/crypto/rsa/rsa_test.c)
 target_link_libraries(rsa_test PUBLIC OpenSSLTest crypto)
 add_dependencies(check rsa_test)
-# add_test(NAME rsa_test COMMAND rsa_test WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR} COMMAND_EXPAND_LISTS)
+add_test(NAME rsa_test COMMAND rsa_test WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR} COMMAND_EXPAND_LISTS)
