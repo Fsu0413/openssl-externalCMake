0a198cd95b1cce3c01cb7e7546a2e060369ff127
Fs <Fsu0413@vip.qq.com>
2024-01-20 15:27:30 +0800

    use convenient variable LINUX and BSD to judge their platform
    Signed-off-by: Fs <Fsu0413@vip.qq.com>

--- crypto/_cryptoDir/objects.cmake
+++ crypto/_cryptoDir/objects.cmake
@@ -87,20 +87,9 @@ if (OPENSSL_ASM)
             x86_64cpuid${OPENSSL_ASM_PREPROCESSED}
         )
     elseif ( ( OPENSSL_TARGET_ARCH STREQUAL "x86" ) AND ( NOT OPENSSL_386 ) )
-        # TODO: properly set up applink and uplink
         set(LIBCRYPTO_CURRENTDIR_ASM_SOURCES
             x86cpuid.S${OPENSSL_ASM_PREPROCESSED_X86}
         )
-        if (false)
-            if ( WIN32 )
-                set(LIBCRYPTO_CURRENTDIR_ASM_SOURCES
-                    ${LIBCRYPTO_CRYPTODIR_ASM_SOURCES}
-                    ${CMAKE_SOURCE_DIR}/ms/applink.c
-                    ${CMAKE_SOURCE_DIR}/ms/uplink.c
-                    uplink-x86.s
-                )
-            endif()
-        endif()
     elseif ( OPENSSL_TARGET_ARCH STREQUAL "arm32" )
         set(LIBCRYPTO_CURRENTDIR_ASM_SOURCES
             armv4cpuid.S
